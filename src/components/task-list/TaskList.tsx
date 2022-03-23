fca37ecbefeb6b09b9928ff98dd0c0b4ea83ded7
PoQuatre <leflem.mateo2002@gmail.com>
2022-03-23 13:28:08 +0100

    feat: display existing tasks

--- src/components/task-list/TaskList.tsx
+++ src/components/task-list/TaskList.tsx
@@ -1,3 +1,20 @@
+import { useContext } from 'react';
+
+import { TasksContext } from 'App';
+
 export const TaskList = () => {
-  return <h1>Task List</h1>;
+  const [tasks, _setTasks] = useContext(TasksContext);
+
+  return (
+    <>
+      <h1>Task List</h1>
+
+      {tasks.map(({ id, title, priority }) => (
+        <div key={id}>
+          <h2>{title}</h2>
+          <p>{priority}</p>
+        </div>
+      ))}
+    </>
+  );
 };
